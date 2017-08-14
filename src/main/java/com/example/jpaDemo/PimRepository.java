1ec89bfe47433dc48e5205783d09394c912fed62
skhollyb <skhollyb@SKPC52301.GARAN.COM>
2017-08-14 11:11:14 -0500

    implement UPC and ship date search in PIM repository

--- src/main/java/com/example/jpaDemo/PimRepository.java
+++ src/main/java/com/example/jpaDemo/PimRepository.java
@@ -1,9 +1,13 @@
 package com.example.jpaDemo;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface PimRepository extends CrudRepository<Pim, Integer>{
-
+	Pim findByUpcAndShipDate(@Param("upc") String poNbr, @Param("shipDate") Date shipDate);
 }
